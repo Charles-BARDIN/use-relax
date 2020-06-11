849d10b65449fdbf7507b34f7f6375b57cf15d4b
Charles <charles.bardin.pro@gmail.com>
2020-06-11 09:57:13 +0200

    use airbnb base config for linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: [
     // /!\ Order matters
     'eslint:recommended',
-    'airbnb',
+    'airbnb/base',
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin.
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier.
     'plugin:prettier/recommended',
